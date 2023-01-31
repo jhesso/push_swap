35bbb87c3b5e28413ad214d2a597e855e29fa50e
Jhesso <juho.hesso@hotmail.com>
2023-01-31 19:38:16 +0200

    working on the duplicate checking. found some problems which are pointed in the comments as well as in the obsidian notes.

--- src/input_validate.c
+++ src/input_validate.c
@@ -6,12 +6,11 @@
 /*   By: jhesso <jhesso@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 11:41:58 by jhesso            #+#    #+#             */
-/*   Updated: 2023/01/31 18:48:54 by jhesso           ###   ########.fr       */
+/*   Updated: 2023/01/31 19:34:49 by jhesso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/pushswap.h"
-#include "../libft/includes/libft.h"
 
 /*	check_duplicates()
 *	checkf if given strings are equal
@@ -19,6 +18,10 @@
 */
 static int	check_duplicates(char *s1, char *s2)
 {
+	if (*s1 == '+')
+		s1++;
+	if (*s2 == '+')
+		s2++;
 	while (*s1 != '\0' || *s2 != '\0')
 	{
 		if (*s1 != *s2)
@@ -34,15 +37,16 @@ static int	check_duplicates(char *s1, char *s2)
 *	No duplicates allowed
 *	return: 1 if arguments are valid, 0 if not.
 */
+// ? should 02 03 04 etc. be counted as 2 3 4... ?
 int	validate_input(char **argv, int argc)
 {
 	int	i;
 	int	j;
 
 	i = 1;
-	j = 2;
 	while (argv[i])
 	{
+		j = i + 1;
 		if (!ft_isnum(argv[i]))
 			return (0);
 		if (argc > 2)
