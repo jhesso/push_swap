f6fab34894c0355ec00e7773371f759dd9a6cb42
Jhesso <juho.hesso@hotmail.com>
2023-02-14 19:10:12 +0200

    changed wording from element to node in comments

--- src/push.c
+++ src/push.c
@@ -6,14 +6,14 @@
 /*   By: jhesso <jhesso@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 14:45:32 by jhesso            #+#    #+#             */
-/*   Updated: 2023/02/14 18:57:53 by jhesso           ###   ########.fr       */
+/*   Updated: 2023/02/14 19:09:52 by jhesso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
 /*	op_push
-*	Pushes the top element of source stack to destination stack
+*	Pushes the top node of source stack to destination stack
 */
 static void	op_push(t_stack **src, t_stack **dst)
 {
@@ -28,7 +28,7 @@ static void	op_push(t_stack **src, t_stack **dst)
 }
 
 /*	op_pa()
-*	Take the first element at the top of b and put it at the top of a.
+*	Take the first node at the top of b and put it at the top of a.
 *	Do nothing if b is empty.
 *	print "pa" on stdout
 */
@@ -39,7 +39,7 @@ void	op_pa(t_stack **stack_a, t_stack **stack_b)
 }
 
 /*	op_pb()
-*	Take the first element at the top of a and put it at the top of b.
+*	Take the first node at the top of a and put it at the top of b.
 *	Do nothing if a is empty.
 *	print "pb" on stdout
 */
